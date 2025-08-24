5e5fb4616e49c13010263b2c829a83d12ca1574f
hata6502 <7702653+hata6502@users.noreply.github.com>
2025-08-24 21:07:31 +0900

    Cosense

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,6 @@ import { PremyDialog } from "./PremyDialog";
 
 const faqs = [
   {
-    title: "コミュニティサイト",
-    url: "https://scrapbox.io/premy/",
-  },
-  {
     title: "油彩ドット絵メーカー",
     url: "https://oil-pixel.hata6502.com/",
   },
@@ -116,6 +112,19 @@ export const App: FunctionComponent<{
             </a>
           </p>
         </footer>
+
+        <Suspense>
+          <div className="mt-32">
+            <Cosenses />
+          </div>
+
+          <div className="mt-16">
+            <Actions
+              isAppInstalled={isAppInstalled}
+              onOpenCanvasButtonClick={handleOpenCanvasButtonClick}
+            />
+          </div>
+        </Suspense>
       </div>
 
       <PremyDialog
@@ -143,7 +152,7 @@ const Actions: FunctionComponent<{
 
     {!isAppInstalled && (
       <a
-        href="https://scrapbox.io/premy/premy%E3%82%A2%E3%83%97%E3%83%AA%E3%82%92%E3%82%B9%E3%83%9E%E3%83%9B%E3%81%AB%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB%E3%81%99%E3%82%8B"
+        href="https://scrapbox.io/hata6502/premy%E3%82%A2%E3%83%97%E3%83%AA%E3%82%92%E3%82%B9%E3%83%9E%E3%83%9B%E3%81%AB%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB%E3%81%99%E3%82%8B"
         target="_blank"
         className="inline-flex items-center gap-x-2 text-sm font-semibold text-gray-900"
       >
@@ -192,3 +201,66 @@ const Tweets: FunctionComponent = () => {
     />
   );
 };
+
+let cosenses:
+  | { projectName: string; title: string; image?: string }[]
+  | undefined;
+const Cosenses: FunctionComponent = () => {
+  if (!cosenses) {
+    throw (async () => {
+      try {
+        const cosenseResponse = await fetch("cosense.json");
+        if (!cosenseResponse.ok) {
+          throw new Error("Failed to fetch cosense.json");
+        }
+        const cosense = await cosenseResponse.json();
+
+        cosenses = shuffled(
+          cosense.relatedPages.links1hop
+            // @ts-expect-error
+            .map((link) => ({ ...link, projectName: "hata6502" }))
+        );
+      } catch (exception) {
+        console.error(exception);
+        cosenses = [];
+      }
+    })();
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-x-4 gap-y-4 md:grid-cols-3">
+      {cosenses.map(({ projectName, title, image }) => (
+        <div key={`${projectName}-${title}`} className="group relative">
+          <span className="text-sm text-gray-700">{title}</span>
+
+          <div className="h-56 w-full overflow-hidden rounded-md group-hover:opacity-75 lg:h-72 xl:h-80">
+            <img
+              src={image}
+              alt={title}
+              className="h-full w-full object-contain object-top"
+            />
+
+            <a
+              href={`https://scrapbox.io/${encodeURIComponent(
+                projectName
+              )}/${encodeURIComponent(title)}`}
+              target="_blank"
+              className="absolute inset-0"
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+function shuffled<Type>(array: Type[]) {
+  const copied = [...array];
+
+  for (let i = copied.length - 1; i >= 1; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copied[i], copied[j]] = [copied[j], copied[i]];
+  }
+
+  return copied;
+}
